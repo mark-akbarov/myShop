3e1f4161605e74d1d025c5cceb8a23f451cf465a
mark-akbarov <mark-akbarov@gmail.com>
2022-03-24 17:55:11 +0500

    Initial Commit

---
+++ myshop/orders/tasks.py
@@ -0,0 +1,17 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+@task
+def order_created(order_id):
+    """
+    Task to send an e-mail notification when an order is
+    successfully created.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = f'Order no. {order.id}'
+    message = f'Dear {order.first_name}, your order has been succesfully placed.\n' \
+              f'Your order ID is {order.id}.'
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+
+    return mail_sent
---
+++ myshop/orders/views.py
@@ -0,0 +1,32 @@
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from .tasks import order_created
+from .models import OrderItem
+from .forms import OrderCreateForm
+from .tasks import order_created
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.discount
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+
+            cart.clear()
+            #launch async task
+            order_created.delay(order.id)
+            #set the order in the session
+            request.session['order_id'] = order.id
+            #redirect for payment
+            return redirect(reverse('payment:process'))
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
